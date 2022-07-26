05a6844b1d7a5c83ddab2bc8b15ab40c02866bd4
g-mc-c <66279681+g-mc-c@users.noreply.github.com>
2022-07-27 00:20:33 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "rt") as fh:
+    long_description = fh.read()
+
+dependencies = [
+    "chia-blockchain==1.3.4",
+    "packaging==21.0",
+    "pytest",
+    "pytest-asyncio",
+    "pytimeparse",
+]
+
+dev_dependencies = [
+    "flake8",
+    "mypy",
+    "black==21.12b0",
+]
+
+setup(
+    name="chia_dev_tools",
+    version="1.0.8",
+    packages=find_packages(exclude=("tests",)),
+    author="Quexington",
+    entry_points={
+        "console_scripts": ["cdv = cdv.cmds.cli:main"],
+    },
+    package_data={
+        "": ["*.clvm", "*.clvm.hex", "*.clib", "*.clsp", "*.clsp.hex"],
+    },
+    author_email="m.hauff@chia.net",
+    setup_requires=["setuptools_scm"],
+    install_requires=dependencies,
+    url="https://github.com/Chia-Network",
+    license="https://opensource.org/licenses/Apache-2.0",
+    description="Chia development commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Security :: Cryptography",
+    ],
+    extras_require=dict(
+        dev=dev_dependencies,
+    ),
+    project_urls={
+        "Bug Reports": "https://github.com/Chia-Network/chia-dev-tools",
+        "Source": "https://github.com/Chia-Network/chia-dev-tools",
+    },
+)
